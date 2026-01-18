5da76d0bdceeb9dcbfe29c97457ee3efd43cb0f1
Evan Maltby <emaltby5@gmail.com>
2026-01-18 17:30:55 -0500

    Fix attendance table to be scrollable and fit to a easier format

--- resources/views/attendanceSheet.blade.php
+++ resources/views/attendanceSheet.blade.php
@@ -3,27 +3,29 @@
 @section('content')
     <h2>Student List</h2>
     <h3>Class Average: {{number_format($average, 2,'.','')}}</h3>
-    @foreach($students as $item)
-        @php
-        $student = $item['student']
-        @endphp
-    <p>
-        <strong> First Name:</strong>{{$student->firstName}}<br>
-        <strong> Last Name:</strong>{{$student->lastName}}<br>
-        <strong> Grade:</strong>{{number_format($student->grade, 2,'.','')}}<br>
-        <strong> Days Missed:</strong>{{$item['days_absent']}}<br>
-    </p>
+        <div style="height: 300px; overflow-y: scroll; border: 1px solid #ccc;">
+            <table>
+                <thead>
+                <tr><th>First Name</th><th>Last Name</th><th>Grade</th><th>Days Missed</th><th>Student Status</th></tr>
+                </thead>
+                <tbody>
+                @foreach ($students as $item)
+                    @php
+                        $student = $item['student']
+                    @endphp
+                    <tr><td>{{ $student->firstName }}</td><td>{{ $student->lastName }}</td><td>{{ number_format($student->grade, 2,'.','') }}</td><td>{{ $item['days_absent'] }}</td>
+                        <td style = "font-weight: bold; color: {{($item['failing'] || $item['truant']) ? 'red' : 'green'}}"> @if($item['truant'])
+                                Student has missed more than 5 days consecutively.
+                            @elseif($item['failing'])
+                                Student currently has a failing grade.
+                            @else
+                                in good standing
+                            @endif</td></tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
 
-    <p style = "font-weight: bold; color: {{($item['failing'] || $item['truant']) ? 'red' : 'green'}}">
-        @if($item['truant'])
-            Student has missed more than 5 days consecutively.
-        @elseif($item['failing'])
-            Student currently has a failing grade.
-        @else
-            in good standing
-        @endif
-    </p>
-    @endforeach
     <a href="{{url('/')}}">
         <button type="button">
             Back
